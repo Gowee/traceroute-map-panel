fc2f8c10f6c0c1f4bfd14bfe4c6dbd01037f183a
Hung-I Wang <whygowe@gmail.com>
2020-05-18 02:45:15 +0800

    Add ipip-cfworker.js

---
+++ ipip-cfworker.js
@@ -0,0 +1,38 @@
+addEventListener('fetch', event => {
+    event.respondWith(cached(handleRequest)(event))
+})
+
+function cached(func) {
+    // Ref: https://github.com/cloudflare/template-registry/blob/f2a21ff87a4f9c60ce1d426e9e8d2e6807b786fd/templates/javascript/cache_api.js#L9
+    const cache = caches.default;
+    async function cachedFunc(event) {
+        const request = event.request;
+        let response = await cache.match(request);
+        if (!response) {
+            response = await func(request);
+            event.waitUntil(cache.put(request, response.clone()));
+        }
+        return response;
+    }
+    return cachedFunc;
+}
+
+async function handleRequest(request) {
+    const url = new URL(request.url);
+    const [, ip] = url.pathname.split("/");
+    if (ip.trim() === "") {
+        return new Response("");
+    }
+    const r = await fetch(`https://btapi.ipip.net/host/info?ip=${ip}&host=Router&lang=EN`, { headers: {'User-Agent': "frosty-waterfall"} });
+    const { as, area } = await r.json();
+    const [region1, region2, region3, _, isp, lat, lon] = area.split("\t");
+    const geo = {
+        ip: ip,
+        label: `${as} ${isp}`.trim(),
+        region: [region1, region2, region3].join(" "),
+        lat: lat ? parseFloat(lat) : undefined,
+        lon: lat ? parseFloat(lon) : undefined,
+        _time: (new Date()).toString()
+    };
+    return new Response(JSON.stringify(geo), { status: 200, headers: { 'Content-Type': "application/json; charset=UTF-8", 'Access-Control-Allow-Origin': "*", 'Cache-Control': "max-age=86400"  } });
+}
